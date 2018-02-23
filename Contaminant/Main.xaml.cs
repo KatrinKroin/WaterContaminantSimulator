d29fdaa4053564d96160a40047fc8d827a2daf0e
Katrin <KatrinKroin@gmail.com>
2018-02-23 16:02:36 +0200

    Project

---
+++ Contaminant/Main.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Contaminant
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        private bool dragStarted = false;
+        double y1 = 1.0;
+        double x1 = 1.0;
+        double y2 = 1.0;
+        double x2 = 1.0;
+        double y3 = 1.0;
+        double x3 = 1.0;
+        double y4 = 1.0;
+        double x4 = 1.0;
+        double[][] grid;
+        public double time;
+        public float del_x;
+        public Main()
+        {
+            InitializeComponent();
+            sldr.Visibility = System.Windows.Visibility.Hidden;
+            grid = new double[500][];
+            for (int i = 0; i < 500; i++)
+            {
+                grid[i] = new double[500];
+            }
+            del_x = 100;
+        }
+
+        private void Picture_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Owner = Window.GetWindow(this);
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.ShowDialog();
+            for (int i=0;i<500;i++)
+            {
+                for (int j = 0; j<500;j++)
+                {
+                    grid[i][j] = window.Grid[i][j];
+                }
+            }
+            if (window.choise == true)
+            {
+                time = window.time;
+                del_x = window.del_x;
+                sldr.Visibility = System.Windows.Visibility.Visible;
+                sldr.Maximum = time;
+                sldr.Value = 0;
+                ChangeImage(0);
+            }
+        }
+
+        private void X_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Y_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            About window = new About();
+            window.Owner = Window.GetWindow(this);
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.ShowDialog();
+        }
+
+        private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            ChangeImage(((Slider)sender).Value);
+            this.dragStarted = false;
+        }
+
+        private void Slider_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            this.dragStarted = true;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (!dragStarted)
+                ChangeImage(e.NewValue);
+        }
+
+        private void ChangeImage(double second)
+        {
+            if (t1 == null) t1 = new TextBlock();
+            if (t2 == null) t2 = new TextBlock();
+            if (t3 == null) t3 = new TextBlock();
+            if (t4 == null) t4 = new TextBlock();
+            float index = Get_index(25);
+            float val = (float)grid[(int)(second/0.2)][(int)index];
+            if (ImageScale == null) ImageScale = new ScaleTransform();
+            ImageScale.ScaleX = x1 + (val * 0.01);
+            ImageScale.ScaleY = y1 + (val * 0.01);
+            t1.Text = " " + string.Format("{0:F2}", val);
+
+
+            index = Get_index(50);
+            val = (float)grid[(int)(second / 0.2)][(int)index];
+            if (im1 == null) im1 = new ScaleTransform();
+            im1.ScaleX = x2 + (val * 0.01);
+            im1.ScaleY = y2 + (val * 0.01);
+            t2.Text = " " + string.Format("{0:F2}", val);
+
+
+            index = Get_index(75);
+            val = (float)grid[(int)(second / 0.2)][(int)index];
+            if (im2 == null) im2 = new ScaleTransform();
+            im2.ScaleX = x3 + (val * 0.01);
+            im2.ScaleY = y3 + (val * 0.01);
+            t3.Text = " " + string.Format("{0:F2}", val);
+
+            index = Get_index(100);
+            val = (float)grid[(int)(second / 0.2)][(int)index];
+            if (im3 == null) im3 = new ScaleTransform();
+            im3.ScaleX = x4 + (val * 0.01);
+            im3.ScaleY = y4 + (val * 0.01);
+            t4.Text = " " + string.Format("{0:F2}", val);
+     
+        }
+
+        private float Get_index(int num)
+        {
+            float index = float.Parse(string.Format("{0:F3}", num / del_x));
+            if(index < 500)
+            {
+                if (index % 10 > 5)
+                {
+                    index = (float)Math.Ceiling(index);
+                }
+                else
+                {
+                    index = (float)Math.Floor(index);
+                }
+            }
+           else index = 499;
+
+            return index;
+        }
+
+    }
+}
---
+++ Contaminant/MainWindow.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Contaminant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private float Lamda = 0;
+        public double[][] Grid;
+        public double time;
+        float del_t;
+        public float del_x;
+        public bool choise;
+        public MainWindow()
+        {
+            InitializeComponent();
+            del_t = 100;
+            del_x = 100;
+            choise = false;
+            Grid = new double[500][];
+            for(int i = 0; i < 500; i++)
+            {
+                Grid[i] = new double[500];
+            }
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void texbox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+
+        private void TextBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox.Text.Equals("Concentration"))
+                txtBox.Text = string.Empty;
+        }
+
+        private void TextBox2_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox.Text.Equals("Velocity"))
+                txtBox.Text = string.Empty;
+        }
+
+        private void TextBox3_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox.Text.Equals("Length of pipe"))
+                txtBox.Text = string.Empty;
+        }
+
+        private void TextBox4_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox.Text.Equals("Initial pollution"))
+                txtBox.Text = string.Empty;
+        }
+
+        private void sel_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox4.Text) || string.IsNullOrWhiteSpace(textBox5.Text) || string.IsNullOrWhiteSpace(textBox3.Text) || textBox4.Text.Equals("Initial pollution") || textBox5.Text.Equals("Velocity") || textBox3.Text.Equals("Amount of pollution"))
+            {
+                MessageBox.Show("Error! One or more fields are missing!");
+            }
+            else
+            {
+                if(float.Parse(textBox4.Text.ToString()) > 100 || float.Parse(textBox3.Text.ToString()) > 100)
+                {
+       
+                    MessageBox.Show("Error! The pollution must be less than 100 milliliters!");
+                }
+                else
+                {
+                    if (float.Parse(textBox5.Text.ToString()) > 100)
+                    {
+                        MessageBox.Show("Error! The velocity must be less than 100 meters in second!");
+                    }
+                    else
+                    {
+                        float init_pol = float.Parse(textBox4.Text.ToString());  //initial pollurion
+                        float vel = float.Parse(textBox5.Text.ToString()); //velocity
+                        float amount_pol = float.Parse(textBox3.Text.ToString()); //amount of pollution
+                        time = sldr.Value;
+                        del_t = (float)(100.00 / 500.00);
+                        if (vel * del_t > 0)
+                        {
+                            del_x = vel * del_t;
+                        }
+                        else del_x = del_t;
+                        Lamda = (float)(vel * (del_t / del_x));
+                        if (Lamda > 1)
+                        {
+                            MessageBox.Show("Unexpected Error!");
+                        }
+                        else
+                        {
+                            Grid_X(init_pol, del_x);
+                            Grid_T(amount_pol, del_t,init_pol);
+                            Fill_Grid();
+                        }
+                        choise = true;
+                        this.Close();
+                    }
+                }
+            }
+        }
+
+
+        public void Grid_X(double poll,double delta_x)
+        {
+            switch (comboBox.SelectedIndex)
+            {
+                case 0:
+                    for(int i = 0; i < 500; i++)
+                    {
+                        Grid[0][i] = poll;
+                    }
+                    break;
+                case 1:
+                    
+                    for(int i = 0; i < 500; i++)
+                    {
+                        Grid[0][i] = i*delta_x + 1.0 + poll;
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < 500; i++)
+                    {
+                        if (Math.Cos(System.Convert.ToDouble(i * delta_x)) < 0)
+                        {
+                            Grid[0][i] = 0 + poll;
+                        }
+                        Grid[0][i] = Math.Cos(System.Convert.ToDouble(i*delta_x)) + poll;
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < 500; i++)
+                    {
+                        if (Math.Sin(System.Convert.ToDouble(i * delta_x)) < 0)
+                        {
+                            Grid[0][i] = 0 + poll;
+                        }
+                        else
+                            Grid[0][i] = Math.Sin(System.Convert.ToDouble(i*delta_x)) + poll;
+                    }
+                    break;
+                case 4:
+                    for (int i = 0; i < 500; i++)
+                    {
+                        Grid[0][i] = Math.Sqrt(i*delta_x) + poll;
+                    }
+                    break;
+            }
+        }
+
+
+
+        public void Grid_T(double fact_poll, float delta_t,float init_poll)
+        {
+            int t = 0;
+            double gr_0_0 = Grid[0][0];
+            for (int i = 1; i < 500; i++)
+            {
+                Grid[i][0] = init_poll;
+            }
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    t = 0;
+                    for (int i = 1; i < 500; i++)
+                    {
+                        t = (int)(Math.Ceiling(i / delta_t));
+                        if (t < 500)
+                            Grid[t][0] = Grid[t][0] + fact_poll;
+                        else break;
+                    }
+                    break;
+                case 1:
+                    //t+2
+                    t = 0;
+                    for (int i = 0; i < 500; i++)
+                    {
+                        t = (int)(Math.Ceiling((i + 2) / delta_t));
+                        if (t<500)
+                            Grid[i][0] = Grid[t][0] + fact_poll;
+                        else break;
+                    }
+                    break;
+                case 2:
+                    //t^2
+                    t = 0;
+                    for (int i = 0; i < 500; i++)
+                    {
+                        t = (int)(Math.Ceiling((i*i)/delta_t));
+                        if (t < 500)
+                            Grid[t][0] = Grid[t][0] + fact_poll;
+                        else break;
+                    }
+                    break;
+            }
+
+        }
+
+        public void Fill_Grid()
+        {
+            for(int i = 1; i < 500; i++)
+            {
+                for(int j = 1; j < 500; j++)
+                {
+                    Grid[i][j] = (1-Lamda)*Grid[i-1][j]+Lamda*Grid[i-1][j-1];
+                }
+            }
+
+        }
+
+
+    }
+
+}
